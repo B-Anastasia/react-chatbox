609821bc81cca5ded49d1494aa5b1259d9508a17
Anastasia <boshniak.a@gmail.com>
2020-06-28 13:40:41 +0300

    Updates

---
+++ precache-manifest.3b5d2ddce94de45005734fb73280de32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e05044c01428c2b6ab7cbb0a53c75f3",
+    "url": "/react-chatbox/index.html"
+  },
+  {
+    "revision": "4fe7eefc98b6044946fa",
+    "url": "/react-chatbox/static/css/main.a8ad434f.chunk.css"
+  },
+  {
+    "revision": "875b005d5391d85b9f72",
+    "url": "/react-chatbox/static/js/2.8c19cb83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-chatbox/static/js/2.8c19cb83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fe7eefc98b6044946fa",
+    "url": "/react-chatbox/static/js/main.3e1409bb.chunk.js"
+  },
+  {
+    "revision": "8324b6cbd44235d22232",
+    "url": "/react-chatbox/static/js/runtime-main.29e47ab9.js"
+  },
+  {
+    "revision": "90ba2bca149f999494d5e8ccd564fc89",
+    "url": "/react-chatbox/static/media/check.90ba2bca.svg"
+  }
+]);
