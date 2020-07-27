486bdd5992c7f75a6d6cf56fee22929531763e55
Anastasia <boshniak.a@gmail.com>
2020-07-27 23:41:34 +0300

    07 - Took out  the constant variable  from the component SelectRadioBox

--- src/components/SelectRadioBox/SelectRadioBox.tsx
+++ src/components/SelectRadioBox/SelectRadioBox.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import Radio from "../common/Radio";
 
+const items = ["React", "Angular", "Redux"];
+
 function SelectRadioBox() {
   const [value, setValue] = useState<string>("");
-  const items = ["React", "Angular", "Redux"];
+
   return (
     <div>
       <Radio items={items} name={"skills"} value={value} onChange={setValue} />
