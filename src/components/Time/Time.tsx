fdd12bbf925198b51c110c075fa6d06803f2595e
Anastasia <boshniak.a@gmail.com>
2020-09-27 22:11:29 +0300

    09 - Added disabled button set interval and stop interval

--- src/components/Time/Time.tsx
+++ src/components/Time/Time.tsx
@@ -1,38 +1,49 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import moment from "moment";
-import { Button } from "../Buttons";
+import {Button} from "../Buttons";
 import scss from "./Time.module.scss";
 
 function Time() {
-  const [time, setTime] = useState(moment().format("LTS"));
-  const [timerId, setTimerId] = useState<number | null>(null);
+    const [time, setTime] = useState(moment().format("LTS"));
+    const [timerId, setTimerId] = useState<number | null>(null);
+    const [activeTime, setActiveTime] = useState<boolean>(false);
+    const runIntervalTime = () => {
+        const new_timerId = window.setInterval(() => {
+            setTime(moment().format("LTS"));
+        }, 1000);
+        setTimerId(new_timerId);
+        setActiveTime(true);
+    };
+    const stopIntervalTime = () => {
+        if (timerId) {
+            clearInterval(timerId);
+            setActiveTime(false);
+        }
+        return;
+    };
+    const btnAct = !activeTime ? scss.active : scss.inactive;
+    const btnInact = activeTime ? scss.active : scss.inactive;
 
-  const runIntervalTime = () => {
-    const new_timerId = window.setInterval(() => {
-      setTime(moment().format("LTS"));
-    }, 1000);
-    setTimerId(new_timerId);
-  };
-  const stopIntervalTime = () => {
-    if (timerId) {
-      clearInterval(timerId);
-    }
-    return;
-  };
-
-  return (
-    <div className={`${scss.block_time} container_sm`}>
-      {/*{data && <div className={scss.data}>{moment().format("LL")}</div>}*/}
-      <div className={scss.time}>
-        <span>{time}</span>
-        <div className={scss.data}>{moment().format("LL")}</div>
-      </div>
-      <div className={scss.buttons}>
-        <Button onClick={runIntervalTime}>set interval</Button>
-        <Button onClick={stopIntervalTime}>stop interval</Button>
-      </div>
-    </div>
-  );
+    return (
+        <div className={`${scss.block_time} container_sm`}>
+            <div className={scss.time}>
+                <span>{time}</span>
+                <div className={scss.data}>{moment().format("LL")}</div>
+            </div>
+            <div className={scss.buttons}>
+                <div className={scss.btn + " " + btnAct}>
+                    <Button onClick={runIntervalTime} disabled={activeTime}>
+                        set interval
+                    </Button>
+                </div>
+                <div className={scss.btn + " " + btnInact}>
+                    <Button onClick={stopIntervalTime} disabled={!activeTime}>
+                        stop interval
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Time;
